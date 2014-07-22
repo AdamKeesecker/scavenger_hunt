ab89f8e94801679f6514d798abcc65c8f91bfc7a
Adam Keesecker <adamkeesecker@gmail.com>
2014-07-22 13:49:47 -0500

    append integer to vector tes written

--- test/vector_test.rb
+++ test/vector_test.rb
@@ -4,8 +4,13 @@ class VectorTest < MiniTest::Unit::TestCase
 
   def test_add_integer_to_vector
     @vector = Vector[1,1]
-    @vector += 1
+    @vector + 1
     assert_equal @vector, [2, 2]
   end
 
+  def test_append_integer_to_vector
+    @vector = Vector[1, 1]
+    @vector << 2
+    assert @vector, Vector[1, 1, 2]
+  end
 end
