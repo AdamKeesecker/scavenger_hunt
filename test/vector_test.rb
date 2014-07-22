70b7aca08da2251d2ba22578290b35db281eb57d
Adam Keesecker <adamkeesecker@gmail.com>
2014-07-22 13:44:08 -0500

    slight test change

--- test/vector_test.rb
+++ test/vector_test.rb
@@ -5,8 +5,7 @@ class VectorTest < MiniTest::Unit::TestCase
   def test_add_integer_to_vector
     @vector = Vector[1,1]
     @vector += 1
-    assert_equal @vector.x, 2
-    assert_equal @vector.y, 2
+    assert_equal @vector, [2, 2]
   end
 
 end
