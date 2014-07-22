8440a5ae6b8760482b296f6d32967200b534cadf
Adam Keesecker <adamkeesecker@gmail.com>
2014-07-22 11:43:23 -0500

    can calculate multiple pathlengths

--- lib/vector.rb
+++ lib/vector.rb
@@ -1,11 +1,20 @@
-class Vector
+require 'matrix'
+
+class MyVector < Vector
   def initialize(x, y)
     self.x = x
     self.y = y
   end
 
-  def calculate(point1, point2)
-    distance = Math.sqrt(((point2.x - point1.x)**) + ((point2.y - point2.y)**))
+  def calculate(point, *others = nil)
+    distance = Math.sqrt(((self[0] - point[0])**2) + ((self[1] - point[1])**2))
+    if others != nil
+      others.each do |vector|
+        i = 1
+        distance += Math.sqrt(((point[i] - vector[i+1])**2) + ((point[i] - vector[i+1])**2))
+        i += 1
+      end
+    end
+    distance
   end
-
 end
