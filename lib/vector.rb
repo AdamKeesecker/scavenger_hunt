fcb6b04bcf8124ff6b0b1d480e1f1a2e06648adb
Adam Keesecker <adamkeesecker@gmail.com>
2014-07-22 11:44:25 -0500

    i was in wrong place. fixed. Multi-pathlength

--- lib/vector.rb
+++ lib/vector.rb
@@ -9,8 +9,8 @@ class MyVector < Vector
   def calculate(point, *others = nil)
     distance = Math.sqrt(((self[0] - point[0])**2) + ((self[1] - point[1])**2))
     if others != nil
+      i = 1
       others.each do |vector|
-        i = 1
         distance += Math.sqrt(((point[i] - vector[i+1])**2) + ((point[i] - vector[i+1])**2))
         i += 1
       end
